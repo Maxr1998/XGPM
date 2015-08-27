1b620b7b3d89b8c65e8f6ff74397e6f17c81eab4
Maxr1998 <max.rumpf1998@gmail.com>
2015-08-27 21:31:59 +0200

    simplifying ArtReplacer

--- app/src/main/java/de/Maxr1998/xposed/gpm/hooks/ArtReplacer.java
+++ app/src/main/java/de/Maxr1998/xposed/gpm/hooks/ArtReplacer.java
@@ -17,21 +17,18 @@ public class ArtReplacer {
 
     public static void init(XC_LoadPackage.LoadPackageParam lPParam) {
         try {
-
-            findAndHookMethod(GPM + ".download.cache.CacheUtils", lPParam.classLoader, "resolvePath", Context.class, String.class, int.class, UUID.class, String.class, new XC_MethodHook() {
+            findAndHookMethod(GPM + ".download.cache.CacheUtils", lPParam.classLoader, "resolveArtPath", Context.class, String.class, int.class, UUID.class, new XC_MethodHook() {
                 @Override
                 protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                    String path;
-                    if (param.getResult() == null || !(path = ((File) param.getResult()).getAbsolutePath()).endsWith(".jpg")) {
+                    if (param.getResult() == null) {
                         return;
                     }
+                    String path = ((File) param.getResult()).getAbsolutePath();
                     String fileName = path.substring(path.lastIndexOf("/") + 1);
                     File replacement = new File(ART_CACHE_OVERLAY_PATH + fileName);
                     if (replacement.exists()) {
                         log("Replacing " + fileName);
                         param.setResult(replacement);
-                    } else {
-                        log(replacement.getAbsolutePath());
                     }
                 }
             });
