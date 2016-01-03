e4a0e91dbacc20a1ca061a0edcabbe14ba3928e5
Maxr1998 <max.rumpf1998@gmail.com>
2016-01-03 23:55:10 +0100

    fix ids

--- app/src/main/java/de/Maxr1998/xposed/gpm/hooks/NavigationDrawer.java
+++ app/src/main/java/de/Maxr1998/xposed/gpm/hooks/NavigationDrawer.java
@@ -32,9 +32,9 @@ public class NavigationDrawer {
                 protected void afterHookedMethod(MethodHookParam param) throws Throwable {
                     Map<Object, Class<?>> map = (Map<Object, Class<?>>) param.args[0];
                     Object playlistsItem = Enum.valueOf(screenClass, "NO_CONTENT");
-                    setIntField(playlistsItem, "mTitleResId", 2131428181);
-                    setIntField(playlistsItem, "mIconResourceId", 0x7f020133);
-                    setIntField(playlistsItem, "mSelectedIconResourceId", 0x7f020135);
+                    setIntField(playlistsItem, "mTitleResId", 0x7f0b0395); // top_menu_playlists
+                    setIntField(playlistsItem, "mIconResourceId", 0x7f02014f); // ic_instant_mix_gray
+                    setIntField(playlistsItem, "mSelectedIconResourceId", 0x7f020150); // ic_instant_mix_orange
                     map.put(playlistsItem, findClass(GPM + ".ui.mylibrary.PlaylistRecyclerFragment", lPParam.classLoader));
                 }
             });
