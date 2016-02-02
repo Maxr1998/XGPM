0c34912766c3356548883fd7fde40b7483620943
Maxr1998 <max.rumpf1998@gmail.com>
2016-02-02 21:15:00 +0100

    fix ClassNotFoundException, catch Exceptions

--- app/src/main/java/de/Maxr1998/xposed/gpm/hooks/NowPlaying.java
+++ app/src/main/java/de/Maxr1998/xposed/gpm/hooks/NowPlaying.java
@@ -37,13 +37,14 @@ import static android.content.res.XModuleResources.createInstance;
 import static de.Maxr1998.xposed.gpm.Common.GPM;
 import static de.Maxr1998.xposed.gpm.hooks.Main.MODULE_PATH;
 import static de.Maxr1998.xposed.gpm.hooks.Main.PREFS;
+import static de.robv.android.xposed.XposedBridge.log;
 import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
 
 public class NowPlaying {
 
     private static XC_LayoutInflated.LayoutInflatedParam exLIPar;
     private static int lastColor = 0;
-    private static Object nowPlayingScreenFragment;
+    private static Object nowPlayingFragment;
     private static ImageButton eQButton;
     private static Palette.PaletteAsyncListener listener = new Palette.PaletteAsyncListener() {
         @Override
@@ -53,141 +54,149 @@ public class NowPlaying {
         }
     };
 
-    public static void init(final XC_LoadPackage.LoadPackageParam lPParam) throws Throwable {
-        // Icon tinting from cover Palette
-        findAndHookMethod(GPM + ".ui.NowPlayingArtPageFragment", lPParam.classLoader, "updateArtVisibility", new XC_MethodHook() {
-            @Override
-            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                PREFS.reload();
-                if (PREFS.getBoolean(Common.NP_TINT_ICONS, false)) {
-                    ImageView mAlbum = (ImageView) XposedHelpers.getObjectField(param.thisObject, "mAlbum");
-                    BitmapDrawable cover = (BitmapDrawable) mAlbum.getDrawable();
-                    if (cover != null) {
-                        Palette.from(cover.getBitmap()).maximumColorCount(16).generate(listener);
-                    }
-                }
-            }
-        });
-        findAndHookMethod(GPM + ".ui.NowPlayingScreenFragment", lPParam.classLoader, "updateQueueSwitcherState", new XC_MethodHook() {
-            @Override
-            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                PREFS.reload();
-                if (PREFS.getBoolean(Common.NP_TINT_ICONS, false)) {
-                    nowPlayingScreenFragment = param.thisObject;
-                    tintGraphics();
-                }
-            }
-        });
-
-        // Handle visibility of EQ Button
-        final String exScrollView = GPM + ".widgets.ExpandingScrollView";
-        final String exState = exScrollView + ".ExpandingState";
-        findAndHookMethod(GPM + ".ui.NowPlayingScreenFragment", lPParam.classLoader, "onExpandingStateChanged", exScrollView, exState, exState, new XC_MethodHook() {
-            @Override
-            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                if (eQButton != null) {
-                    eQButton.setVisibility(((View) XposedHelpers.getObjectField(param.thisObject, "mQueueSwitcher")).getVisibility());
-                }
-            }
-        });
-    }
-
-    public static void initResources(final XC_InitPackageResources.InitPackageResourcesParam resParam) throws Throwable {
-        final XModuleResources modRes = createInstance(MODULE_PATH, resParam.res);
-
-        // Replace overflow button
-        resParam.res.setReplacement(GPM, "drawable", "ic_menu_moreoverflow_large", modRes.fwd(R.drawable.ic_more_vert_black_24dp));
-
-        // Remove drop shadow from album art
-        if (PREFS.getBoolean(Common.NP_REMOVE_DROP_SHADOW, false)) {
-            resParam.res.setReplacement(GPM, "drawable", "now_playing_art_scrim", new XResources.DrawableLoader() {
+    public static void init(final XC_LoadPackage.LoadPackageParam lPParam) {
+        try {
+            // Icon tinting from cover Palette
+            findAndHookMethod(GPM + ".ui.NowPlayingArtPageFragment", lPParam.classLoader, "updateArtVisibility", new XC_MethodHook() {
                 @Override
-                public Drawable newDrawable(XResources xResources, int i) throws Throwable {
-                    return new ColorDrawable(0);
-                }
-            });
-            resParam.res.setReplacement(GPM, "dimen", "rating_controls_now_playing_page", modRes.fwd(R.dimen.controls_height));
-        }
-
-        resParam.res.hookLayout(GPM, "layout", "nowplaying_screen", new XC_LayoutInflated() {
-            @Override
-            public void handleLayoutInflated(LayoutInflatedParam lIParam) throws Throwable {
-                PREFS.reload();
-                exLIPar = lIParam;
-                // Global vars
-                RelativeLayout header = (RelativeLayout) lIParam.view.findViewById(lIParam.res.getIdentifier("top_wrapper_right", "id", GPM));
-                View queueSwitcher = header.findViewById(lIParam.res.getIdentifier("queue_switcher", "id", GPM));
-                View overflow = header.findViewById(lIParam.res.getIdentifier("overflow", "id", GPM));
-
-                // Touch feedback
-                @SuppressLint("InlinedApi") TypedArray a = header.getContext().obtainStyledAttributes(new int[]{Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP
-                        ? android.R.attr.selectableItemBackgroundBorderless : android.R.attr.selectableItemBackground});
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
-                    queueSwitcher.setBackground(a.getDrawable(0));
-                    overflow.setBackground(a.getDrawable(0));
-                } else {
-                    //noinspection deprecation
-                    queueSwitcher.setBackgroundDrawable(a.getDrawable(0));
-                    //noinspection deprecation
-                    overflow.setBackgroundDrawable(a.getDrawable(0));
-                }
-
-                // Add EQ button
-                if (PREFS.getBoolean(Common.NP_ADD_EQ_SHORTCUT, false)) {
-                    header.addView(getEQButton(header, resParam.res), 0);
-                    RelativeLayout.LayoutParams queueParams = (RelativeLayout.LayoutParams) queueSwitcher.getLayoutParams();
-                    queueParams.addRule(RelativeLayout.RIGHT_OF, 0);
-                    queueParams.addRule(RelativeLayout.RIGHT_OF, lIParam.res.getIdentifier("plain", "id", GPM));
-                    queueParams.setMargins(0, 0, 0, 0);
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
-                        queueParams.setMarginStart(0);
-                        eQButton.setBackground(a.getDrawable(0));
-                    } else {
-                        //noinspection deprecation
-                        eQButton.setBackgroundDrawable(a.getDrawable(0));
-                    }
-                    eQButton.setVisibility(queueSwitcher.getVisibility());
-                }
-                a.recycle();
-
-                // Resize covers
-                if (PREFS.getBoolean(Common.NP_RESIZE_COVERS, false)) {
-                    View pager = lIParam.view.findViewById(lIParam.res.getIdentifier("art_pager", "id", GPM));
-                    RelativeLayout.LayoutParams pagerLayoutParams = (RelativeLayout.LayoutParams) pager.getLayoutParams();
-                    pagerLayoutParams.addRule(RelativeLayout.BELOW, lIParam.res.getIdentifier("header_pager", "id", GPM));
-                    pagerLayoutParams.addRule(RelativeLayout.ABOVE, lIParam.res.getIdentifier("play_controls", "id", GPM));
-                    pager.setLayoutParams(pagerLayoutParams);
-                }
-                // Improve playback controls visibility
-                if (PREFS.getBoolean(Common.NP_REMOVE_DROP_SHADOW, false)) {
-                    ImageView repeat = (ImageView) lIParam.view.findViewById(lIParam.res.getIdentifier("repeat", "id", GPM)),
-                            shuffle = (ImageView) lIParam.view.findViewById(lIParam.res.getIdentifier("shuffle", "id", GPM));
-                    if (((View) repeat.getParent()).getId() != lIParam.res.getIdentifier("play_controls", "id", GPM)) {
-                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                            repeat.setBackground(modRes.getDrawable(R.drawable.ripple_circle, null));
-                            shuffle.setBackground(modRes.getDrawable(R.drawable.ripple_circle, null));
-                        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
-                            //noinspection deprecation
-                            repeat.setBackground(modRes.getDrawable(R.drawable.circle));
-                            //noinspection deprecation
-                            shuffle.setBackground(modRes.getDrawable(R.drawable.circle));
-                        } else {
-                            //noinspection deprecation
-                            repeat.setBackgroundDrawable(modRes.getDrawable(R.drawable.circle));
-                            //noinspection deprecation
-                            shuffle.setBackgroundDrawable(modRes.getDrawable(R.drawable.circle));
+                protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                    PREFS.reload();
+                    if (PREFS.getBoolean(Common.NP_TINT_ICONS, false)) {
+                        ImageView mAlbum = (ImageView) XposedHelpers.getObjectField(param.thisObject, "mAlbum");
+                        BitmapDrawable cover = (BitmapDrawable) mAlbum.getDrawable();
+                        if (cover != null) {
+                            Palette.from(cover.getBitmap()).maximumColorCount(16).generate(listener);
                         }
                     }
                 }
-                // Tint graphics
-                if (PREFS.getBoolean(Common.NP_TINT_ICONS, false)) {
-                    tintGraphics();
+            });
+            findAndHookMethod(GPM + ".ui.NowPlayingFragment", lPParam.classLoader, "updateQueueSwitcherState", new XC_MethodHook() {
+                @Override
+                protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                    PREFS.reload();
+                    if (PREFS.getBoolean(Common.NP_TINT_ICONS, false)) {
+                        nowPlayingFragment = param.thisObject;
+                        tintGraphics();
+                    }
                 }
-            }
-        });
+            });
+
+            // Handle visibility of EQ Button
+            final String exScrollView = GPM + ".widgets.ExpandingScrollView";
+            final String exState = exScrollView + ".ExpandingState";
+            findAndHookMethod(GPM + ".ui.NowPlayingFragment", lPParam.classLoader, "onExpandingStateChanged", exScrollView, exState, exState, new XC_MethodHook() {
+                @Override
+                protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                    if (eQButton != null) {
+                        eQButton.setVisibility(((View) XposedHelpers.getObjectField(param.thisObject, "mQueueSwitcher")).getVisibility());
+                    }
+                }
+            });
+        } catch (Throwable t) {
+            log(t);
+        }
     }
 
-    private static ImageButton getEQButton(RelativeLayout header, XResources res) {
+    public static void initResources(final XC_InitPackageResources.InitPackageResourcesParam resParam) {
+        try {
+            final XModuleResources modRes = createInstance(MODULE_PATH, resParam.res);
+
+            // Replace overflow button
+            resParam.res.setReplacement(GPM, "drawable", "ic_menu_moreoverflow_large", modRes.fwd(R.drawable.ic_more_vert_black_24dp));
+
+            // Remove drop shadow from album art
+            if (PREFS.getBoolean(Common.NP_REMOVE_DROP_SHADOW, false)) {
+                resParam.res.setReplacement(GPM, "drawable", "now_playing_art_scrim", new XResources.DrawableLoader() {
+                    @Override
+                    public Drawable newDrawable(XResources xResources, int i) throws Throwable {
+                        return new ColorDrawable(0);
+                    }
+                });
+                resParam.res.setReplacement(GPM, "dimen", "rating_controls_now_playing_page", modRes.fwd(R.dimen.controls_height));
+            }
+
+            resParam.res.hookLayout(GPM, "layout", "nowplaying_screen", new XC_LayoutInflated() {
+                @Override
+                public void handleLayoutInflated(LayoutInflatedParam lIParam) throws Throwable {
+                    PREFS.reload();
+                    exLIPar = lIParam;
+                    // Global vars
+                    RelativeLayout header = (RelativeLayout) lIParam.view.findViewById(lIParam.res.getIdentifier("top_wrapper_right", "id", GPM));
+                    View queueSwitcher = header.findViewById(lIParam.res.getIdentifier("queue_switcher", "id", GPM));
+                    View overflow = header.findViewById(lIParam.res.getIdentifier("overflow", "id", GPM));
+
+                    // Touch feedback
+                    @SuppressLint("InlinedApi") TypedArray a = header.getContext().obtainStyledAttributes(new int[]{Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP
+                            ? android.R.attr.selectableItemBackgroundBorderless : android.R.attr.selectableItemBackground});
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+                        queueSwitcher.setBackground(a.getDrawable(0));
+                        overflow.setBackground(a.getDrawable(0));
+                    } else {
+                        //noinspection deprecation
+                        queueSwitcher.setBackgroundDrawable(a.getDrawable(0));
+                        //noinspection deprecation
+                        overflow.setBackgroundDrawable(a.getDrawable(0));
+                    }
+
+                    // Add EQ button
+                    if (PREFS.getBoolean(Common.NP_ADD_EQ_SHORTCUT, false)) {
+                        header.addView(getEQButton(header, resParam.res), 0);
+                        RelativeLayout.LayoutParams queueParams = (RelativeLayout.LayoutParams) queueSwitcher.getLayoutParams();
+                        queueParams.addRule(RelativeLayout.RIGHT_OF, 0);
+                        queueParams.addRule(RelativeLayout.RIGHT_OF, lIParam.res.getIdentifier("plain", "id", GPM));
+                        queueParams.setMargins(0, 0, 0, 0);
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+                            queueParams.setMarginStart(0);
+                            eQButton.setBackground(a.getDrawable(0));
+                        } else {
+                            //noinspection deprecation
+                            eQButton.setBackgroundDrawable(a.getDrawable(0));
+                        }
+                        eQButton.setVisibility(queueSwitcher.getVisibility());
+                    }
+                    a.recycle();
+
+                    // Resize covers
+                    if (PREFS.getBoolean(Common.NP_RESIZE_COVERS, false)) {
+                        View pager = lIParam.view.findViewById(lIParam.res.getIdentifier("art_pager", "id", GPM));
+                        RelativeLayout.LayoutParams pagerLayoutParams = (RelativeLayout.LayoutParams) pager.getLayoutParams();
+                        pagerLayoutParams.addRule(RelativeLayout.BELOW, lIParam.res.getIdentifier("header_pager", "id", GPM));
+                        pagerLayoutParams.addRule(RelativeLayout.ABOVE, lIParam.res.getIdentifier("play_controls", "id", GPM));
+                        pager.setLayoutParams(pagerLayoutParams);
+                    }
+                    // Improve playback controls visibility
+                    if (PREFS.getBoolean(Common.NP_REMOVE_DROP_SHADOW, false)) {
+                        ImageView repeat = (ImageView) lIParam.view.findViewById(lIParam.res.getIdentifier("repeat", "id", GPM)),
+                                shuffle = (ImageView) lIParam.view.findViewById(lIParam.res.getIdentifier("shuffle", "id", GPM));
+                        if (((View) repeat.getParent()).getId() != lIParam.res.getIdentifier("play_controls", "id", GPM)) {
+                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                                repeat.setBackground(modRes.getDrawable(R.drawable.ripple_circle, null));
+                                shuffle.setBackground(modRes.getDrawable(R.drawable.ripple_circle, null));
+                            } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                                //noinspection deprecation
+                                repeat.setBackground(modRes.getDrawable(R.drawable.circle));
+                                //noinspection deprecation
+                                shuffle.setBackground(modRes.getDrawable(R.drawable.circle));
+                            } else {
+                                //noinspection deprecation
+                                repeat.setBackgroundDrawable(modRes.getDrawable(R.drawable.circle));
+                                //noinspection deprecation
+                                shuffle.setBackgroundDrawable(modRes.getDrawable(R.drawable.circle));
+                            }
+                        }
+                    }
+                    // Tint graphics
+                    if (PREFS.getBoolean(Common.NP_TINT_ICONS, false)) {
+                        tintGraphics();
+                    }
+                }
+            });
+        } catch (Throwable t) {
+            log(t);
+        }
+    }
+
+    private static ImageButton getEQButton(RelativeLayout header, XResources res) throws Throwable {
         eQButton = new ImageButton(header.getContext());
         eQButton.setId(exLIPar.res.getIdentifier("plain", "id", GPM));
         RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(
@@ -243,11 +252,11 @@ public class NowPlaying {
         ((TextView) exLIPar.view.findViewById(exLIPar.res.getIdentifier("currenttime", "id", GPM))).setTextColor(lastColor);
         playPause.getBackground().setColorFilter(lastColor, PorterDuff.Mode.SRC_ATOP);
 
-        if (nowPlayingScreenFragment == null) {
+        if (nowPlayingFragment == null) {
             return;
         }
         ImageButton queue = (ImageButton) exLIPar.view.findViewById(exLIPar.res.getIdentifier("queue_switcher", "id", GPM));
-        if (XposedHelpers.getBooleanField(nowPlayingScreenFragment, "mQueueShown")) {
+        if (XposedHelpers.getBooleanField(nowPlayingFragment, "mQueueShown")) {
             queue.setColorFilter(lastColor);
         } else queue.clearColorFilter();
     }
