33081cf063732a442b007b5f05f63d2a348d8ccf
Maxr1998 <max.rumpf1998@gmail.com>
2017-01-24 19:34:52 +0100

    Enable new and experimental features and settings

--- app/src/main/java/de/Maxr1998/xposed/gpm/hooks/Features.java
+++ app/src/main/java/de/Maxr1998/xposed/gpm/hooks/Features.java
@@ -11,39 +11,64 @@ import static de.Maxr1998.xposed.gpm.Common.GPM;
 import static de.robv.android.xposed.XposedBridge.log;
 import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
 import static de.robv.android.xposed.XposedHelpers.findClass;
+import static de.robv.android.xposed.XposedHelpers.getStaticObjectField;
+import static de.robv.android.xposed.XposedHelpers.setObjectField;
 import static de.robv.android.xposed.XposedHelpers.setStaticBooleanField;
 
 class Features {
 
     public static void init(final XC_LoadPackage.LoadPackageParam lPParam) {
         try {
-            findAndHookMethod(GPM + ".Feature", lPParam.classLoader, "isFullWidthSearchEnabled", new XC_MethodReplacement() {
-                @Override
-                protected Object replaceHookedMethod(XC_MethodHook.MethodHookParam param) throws Throwable {
-                    return true;
-                }
-            });
+            // Features
+            String[] noContextMethods = new String[]{"isFullWidthSearchEnabled", "isSleepTimerEnabled"};
+            for (int i = 0; i < noContextMethods.length; i++) {
+                findAndHookMethod(GPM + ".Feature", lPParam.classLoader, noContextMethods[i], new XC_MethodReplacement() {
+                    @Override
+                    protected Object replaceHookedMethod(XC_MethodHook.MethodHookParam param) throws Throwable {
+                        return true;
+                    }
+                });
+            }
 
-            findAndHookMethod(GPM + ".Feature", lPParam.classLoader, "isHeadphoneNotificationAvailableForUser", Context.class, new XC_MethodReplacement() {
-                @Override
-                protected Object replaceHookedMethod(XC_MethodHook.MethodHookParam param) throws Throwable {
-                    return true;
-                }
-            });
+            String[] contextMethods = new String[]{"isHeadphoneNotificationAvailableForUser", "isHeadphoneNotificationBroadcastReceiverEnabled", "isSoundSearchEnabled"};
+            for (int i = 0; i < contextMethods.length; i++) {
+                findAndHookMethod(GPM + ".Feature", lPParam.classLoader, contextMethods[i], Context.class, new XC_MethodReplacement() {
+                    @Override
+                    protected Object replaceHookedMethod(XC_MethodHook.MethodHookParam param) throws Throwable {
+                        return true;
+                    }
+                });
+            }
 
-            findAndHookMethod(GPM + ".Feature", lPParam.classLoader, "isSleepTimerEnabled", new XC_MethodReplacement() {
-                @Override
-                protected Object replaceHookedMethod(XC_MethodHook.MethodHookParam param) throws Throwable {
-                    return true;
-                }
-            });
+            // To allow headphone notification
+            setObjectField(getStaticObjectField(findClass(GPM + ".Feature", lPParam.classLoader), "head"), "buildFlag", true);
 
-            findAndHookMethod(GPM + ".Feature", lPParam.classLoader, "isSoundSearchEnabled", Context.class, new XC_MethodReplacement() {
-                @Override
-                protected Object replaceHookedMethod(XC_MethodHook.MethodHookParam param) throws Throwable {
-                    return true;
-                }
-            });
+            // Experiments
+            String[] experimentsMethods = new String[]{"canShowRemoteSearchToFreeUsersInSubscriptionCountries", "isAutoPlayLastSongEnabled",
+                    "isFastFirstTrackControl", "isFastFirstTrackExperiment", "isTopChartsIcingForNonWoodstockCountriesEnabled",
+                    "isTopChartsIcingForWoodstockCountriesEnabled", "shouldShowTopChartsAndNewReleasesToAllInSubscriptionCountries"};
+            for (int i = 0; i < experimentsMethods.length; i++) {
+                findAndHookMethod(GPM + ".experiments.AnalysisExperimentsManager", lPParam.classLoader, experimentsMethods[i], new XC_MethodReplacement() {
+                    @Override
+                    protected Object replaceHookedMethod(XC_MethodHook.MethodHookParam param) throws Throwable {
+                        return true;
+                    }
+                });
+            }
+
+            /*if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                findAndHookMethod(GPM + ".experiments.AnalysisExperimentsManager", lPParam.classLoader, "getFifeQualityBucket", new XC_MethodReplacement() {
+                    @TargetApi(Build.VERSION_CODES.N)
+                    @Override
+                    protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
+                        //noinspection Since15
+                        return java.util.Optional.of(2);
+                    }
+                });
+            }*/
+
+            // Flags: Just trying this out :P
+            setStaticBooleanField(findClass(GPM + ".Flag", lPParam.classLoader), "UI_TEST_BUILD", true);
 
             // Gservices overrides
             findAndHookMethod("com.google.android.gsf.Gservices", lPParam.classLoader, "getBoolean", ContentResolver.class, String.class, boolean.class, new XC_MethodHook() {
@@ -56,9 +81,6 @@ class Features {
                     }
                 }
             });
-
-            // Just trying out :P
-            setStaticBooleanField(findClass(GPM + ".Flag", lPParam.classLoader), "UI_TEST_BUILD", true);
         } catch (Throwable t) {
             log(t);
         }
